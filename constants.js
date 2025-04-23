4ba9d10531d450f2b1199ccbeb62556025aa0245
Muzeffer <muzeffer9b2004@gmail.com>
2025-04-24 02:36:56 +0400

    Final commit

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 export const API_KEY = "4b2abe80b8549c338fd31e3e";
-export const DELAY_TIME = 2000;
+export const DELAY_TIME = 1000;
--- script.js
+++ script.js
@@ -6,41 +6,62 @@ const input1 = converter1.querySelector("input");
 const input2 = converter2.querySelector("input");
 const span1 = converter1.querySelector(".input span");
 const span2 = converter2.querySelector(".input span");
+const errorDiv = document.querySelector(".error");
+const errorMessage = errorDiv.querySelector("p");
+const hamburgerContainer = document.querySelector(".hamburger");
+const hamburgerBtn = hamburgerContainer.querySelector("button");
+const hamburgerDropdown = hamburgerContainer.querySelector(".dropdown");
 import { API_KEY } from "./constants.js";
 import { DELAY_TIME } from "./constants.js";
 
-//RESPONSIVLIYY QALIBBB
+//RESPONSIVLIYY TELEFON UCUN
 
 let currency1 = "RUB";
 let currency2 = "USD";
 let input1Timer;
 let input2Timer;
+let lastChanged = "input1";
 
 const convertCurrency = async (currency) => {
   try {
     const response = await fetch(
       `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currency}`
     );
+    if (!response.ok) throw new Error();
     const data = await response.json();
+    hideError();
     return data;
   } catch (error) {
-    console.error(error);
+    showError(
+      "Valyuta məlumatlarını əldə etmək mümkün olmadı. Zəhmət olmasa internet bağlantınızın olduğundan əmin olun!"
+    );
+    throw error;
   }
 };
 
-const handleCurrencyDivClick = (currencyDiv, currencyValue) => {
+const handleCurrencyDivClick = (currencyDiv, setCurrency) => {
   currencyDiv.forEach((currency) => {
-    currency.addEventListener("click", () => {
-      if (currency.textContent === currencyValue) return;
+    currency.addEventListener("click", async () => {
+      const selectedCurrency = currency.textContent;
+      if (selectedCurrency === setCurrency()) return;
+
       currencyDiv.forEach((el) => el.classList.remove("active"));
-      currencyValue = currency.textContent;
       currency.classList.add("active");
+      setCurrency(selectedCurrency);
+      await updateForNewCurrency();
     });
   });
 };
 
-handleCurrencyDivClick(currencyDivs1, currency1);
-handleCurrencyDivClick(currencyDivs2, currency2);
+handleCurrencyDivClick(currencyDivs1, (value) => {
+  if (value !== undefined) currency1 = value;
+  return currency1;
+});
+
+handleCurrencyDivClick(currencyDivs2, (value) => {
+  if (value !== undefined) currency2 = value;
+  return currency2;
+});
 
 const formatInputs = (input) => {
   input.addEventListener("focus", () => {
@@ -60,27 +81,127 @@ formatInputs(input1);
 formatInputs(input2);
 
 input1.addEventListener("input", () => {
-  let data;
   clearTimeout(input1Timer);
   if (input1.value.trim() === "") return;
+  lastChanged = "input1";
 
   input1Timer = setTimeout(async () => {
-    data = await convertCurrency(currency1);
-    console.log(data);
-    const convertedAmount = input1.value * data.conversion_rates[currency2];
-    input2.value = convertedAmount.toFixed(5);
+    if (currency2 === currency1) {
+      input2.value = Number(input1.value).toFixed(5);
+    } else {
+      const data = await convertCurrency(currency1);
+      const amount = input1.value * data.conversion_rates[currency2];
+      input2.value = amount.toFixed(5);
+    }
   }, DELAY_TIME);
 });
 
 input2.addEventListener("input", () => {
-  let data;
   clearTimeout(input2Timer);
   if (input2.value.trim() === "") return;
+  lastChanged = "input2";
 
   input2Timer = setTimeout(async () => {
-    data = await convertCurrency(currency2);
-    console.log(data);
-    const amount = input2.value * data.conversion_rates[currency1];
-    input1.value = amount.toFixed(5);
+    if (currency1 === currency2) {
+      input1.value = Number(input2.value).toFixed(5);
+    } else {
+      const data = await convertCurrency(currency2);
+      const amount = input2.value * data.conversion_rates[currency1];
+      input1.value = amount.toFixed(5);
+    }
   }, DELAY_TIME);
+});
+
+const updateForNewCurrency = async () => {
+  if (currency1 === currency2) {
+    span1.textContent = `1 ${currency1} = 1 ${currency2}`;
+    span2.textContent = `1 ${currency2} = 1 ${currency1}`;
+
+    if (lastChanged === "input1") {
+      const value = +input1.value;
+      console.log(value);
+      input2.value = value ? value.toFixed(5) : 0;
+    } else {
+      const value = +input2.value;
+      input1.value = value ? value.toFixed(5) : 0;
+    }
+    return;
+  }
+
+  if (lastChanged === "input1") {
+    const data = await convertCurrency(currency1);
+    const currencyRate = data.conversion_rates[currency2];
+    const reversedRate = 1 / currencyRate;
+    const amount = input1.value * currencyRate;
+    input2.value = amount ? amount.toFixed(5) : 0;
+    span1.textContent = `1 ${currency1} = ${currencyRate.toFixed(
+      5
+    )} ${currency2}`;
+    span2.textContent = `1 ${currency2} = ${reversedRate.toFixed(
+      5
+    )} ${currency1}`;
+  } else {
+    const data = await convertCurrency(currency2);
+    const currencyRate = data.conversion_rates[currency1];
+    const reversedRate = 1 / currencyRate;
+    const amount = input2.value * currencyRate;
+    input1.value = amount ? amount.toFixed(5) : 0;
+    span1.textContent = `1 ${currency1} = ${currencyRate.toFixed(
+      5
+    )} ${currency2}`;
+    span2.textContent = `1 ${currency2} = ${reversedRate.toFixed(
+      5
+    )} ${currency1}`;
+  }
+};
+
+updateForNewCurrency();
+
+const formatInputValue = (input) => {
+  input.addEventListener("input", () => {
+    const parts = input.value.split(".");
+    if (parts[1] && parts[1].length > 5) {
+      parts[1] = parts[1].slice(0, 5);
+      input.value = parts.join(".");
+    }
+  });
+};
+
+formatInputValue(input1);
+formatInputValue(input2);
+
+hamburgerBtn.addEventListener("click", () => {
+  if (hamburgerDropdown.classList.contains("hide")) {
+    hamburgerDropdown.classList.remove("hide");
+    hamburgerDropdown.classList.add("show");
+  } else {
+    hamburgerDropdown.classList.add("hide");
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (
+    window.innerWidth > 1100 &&
+    hamburgerDropdown.classList.contains("show")
+  ) {
+    hamburgerDropdown.classList.add("hide");
+  }
+});
+
+const showError = (message) => {
+  errorMessage.textContent = message;
+  errorDiv.style.display = "flex";
+};
+
+const hideError = () => {
+  errorDiv.style.display = 'none';
+};
+
+window.addEventListener("online", async () => {
+  hideError();
+  await updateForNewCurrency();
+});
+
+window.addEventListener("offline", () => {
+  showError("Internet bağlantınız yoxdur!");
 });
